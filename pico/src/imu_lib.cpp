1d8473fcb993a41d6a21904a2cbea4ef771ec86c
MythicalNewt <89124240+MythicalNewt@users.noreply.github.com>
2026-03-19 20:22:19 +0530

    Debugged IMU code.

--- pico/src/imu_lib.cpp
+++ pico/src/imu_lib.cpp
@@ -10,9 +10,11 @@ bool IMU::begin(TwoWire *bus){
     }
 
     icm.setAccelRange(ICM20649_ACCEL_RANGE_16_G);   // Allowed: 4, 8, 16, 30
-    icm.setGyroRange(ICM20649_GYRO_RANGE_2000_DPS); // Allowed 500, 1000, 2000, 4000
-    icm.setAccelRateDivisor(4);                     // accel_rate = 1125 / (1 + accel_divisor)
-    icm.setGyroRateDivisor(4);                      // gyro_rate = 1100 / (1 + gyro_divisor)
+    icm.setGyroRange(ICM20649_GYRO_RANGE_2000_DPS); // Allowed: 500, 1000, 2000, 4000
+    icm.setAccelRateDivisor(4);                     //  @225Hz accel_rate = 1125 / (1 + accel_divisor)
+    icm.setGyroRateDivisor(4);                      //  @220Hz gyro_rate = 1100 / (1 + gyro_divisor)
+
+    return true;
     /*
     Might want to set a higher I2C bus speed
     We might poll the icm for avg/lowpass filtering
@@ -23,9 +25,6 @@ bool IMU::begin(TwoWire *bus){
 }
 
 void IMU::calibrate(){
-    sensors_event_t accel;
-    sensors_event_t gyro;
-
     float sum_gx = 0, sum_gy = 0, sum_gz = 0;
     float sum_ax = 0, sum_ay = 0, sum_az = 0;
 
@@ -34,28 +33,28 @@ void IMU::calibrate(){
     for (uint16_t i = 0; i < SAMPLES; i++)
     {
 
-        icm.getEvent(&accel, &gyro, NULL);
+        icm.getEvent(&accel_event, &gyro_event, NULL);
 
-        sum_gx += gyro.gyro.x;
-        sum_gy += gyro.gyro.y;
-        sum_gz += gyro.gyro.z;
+        sum_gx += gyro_event.gyro.x;
+        sum_gy += gyro_event.gyro.y;
+        sum_gz += gyro_event.gyro.z;
 
-        sum_ax += accel.acceleration.x;
-        sum_ay += accel.acceleration.y;
-        sum_az += accel.acceleration.z;
+        sum_ax += accel_event.acceleration.x;
+        sum_ay += accel_event.acceleration.y;
+        sum_az += accel_event.acceleration.z;
 
         delay(DELAY); // running @200Hz
     }
 
     // -------- Gyro bias --------
-    gyro_bias.x = sum_gx / samples;
-    gyro_bias.y = sum_gy / samples;
-    gyro_bias.z = sum_gz / samples;
+    gyro_bias.x = sum_gx / SAMPLES;
+    gyro_bias.y = sum_gy / SAMPLES;
+    gyro_bias.z = sum_gz / SAMPLES;
 
     // -------- Accelerometer bias --------
-    float avg_ax = sum_ax / samples;
-    float avg_ay = sum_ay / samples;
-    float avg_az = sum_az / samples;
+    float avg_ax = sum_ax / SAMPLES;
+    float avg_ay = sum_ay / SAMPLES;
+    float avg_az = sum_az / SAMPLES;
 
     // Rocket upright assumption:
     // X ≈ 0
@@ -68,36 +67,29 @@ void IMU::calibrate(){
 }
 
 void IMU::getAccelGyroTemp(Vec3 *a, Vec3 *g, float* t){
-    sensors_event_t accel;
-    sensors_event_t gyro;
-    sensors_event_t temp;
-
-    icm.getEvent(&accel, &gyro, &temp);
+    icm.getEvent(&accel_event, &gyro_event, &temp_event);
     
-    a->x = accel.acceleration.x - accel_bias.x;
-    a->z = accel.acceleration.z - accel_bias.z;
-    a->y = accel.acceleration.y - accel_bias.y;
+    a->x = accel_event.acceleration.x - accel_bias.x;
+    a->y = accel_event.acceleration.y - accel_bias.y;
+    a->z = accel_event.acceleration.z - accel_bias.z;
 
-    g->x = gyro.gyro.x - gyro_bias.x;
-    g->y = gyro.gyro.y - gyro_bias.y;
-    g->z = gyro.gyro.z - gyro_bias.z;
+    g->x = gyro_event.gyro.x - gyro_bias.x;
+    g->y = gyro_event.gyro.y - gyro_bias.y;
+    g->z = gyro_event.gyro.z - gyro_bias.z;
 
-    *t = temp.temperature;
+    *t = temp_event.temperature;
 }
 
 void IMU::getAccelGyro(Vec3 *a, Vec3 *g){  // Store these!!
-    sensors_event_t accel;
-    sensors_event_t gyro;
-
-    icm.getEvent(&accel, &gyro, NULL);
+    icm.getEvent(&accel_event, &gyro_event, NULL);
     
-    a->x = accel.acceleration.x - accel_bias.x;
-    a->z = accel.acceleration.z - accel_bias.z;
-    a->y = accel.acceleration.y - accel_bias.y;
+    a->x = accel_event.acceleration.x - accel_bias.x;
+    a->y = accel_event.acceleration.y - accel_bias.y;
+    a->z = accel_event.acceleration.z - accel_bias.z;
 
-    g->x = gyro.gyro.x - gyro_bias.x;
-    g->y = gyro.gyro.y - gyro_bias.y;
-    g->z = gyro.gyro.z - gyro_bias.z;
+    g->x = gyro_event.gyro.x - gyro_bias.x;
+    g->y = gyro_event.gyro.y - gyro_bias.y;
+    g->z = gyro_event.gyro.z - gyro_bias.z;
 }
 // need to get gyro orientation to make local coordinates into world
 // see arduino std madgwick quarternion library
--- pico/src/imu_lib.h
+++ pico/src/imu_lib.h
@@ -5,6 +5,12 @@
 #include <Arduino.h>
 #include <Wire.h>
 
+struct Vec3{
+    float x;
+    float y;
+    float z;
+};
+
 class IMU{
     public:
     bool begin(TwoWire* bus);
@@ -12,18 +18,17 @@ class IMU{
     void getAccelGyroTemp(Vec3 *accel, Vec3 *gyro, float* t);
     void getAccelGyro(Vec3 *a, Vec3 *g);
 
-    struct Vec3{
-        float x;
-        float y;
-        float z;
-    };
     
     private:
     Adafruit_ICM20649 icm;
     Vec3 gyro_bias = {0.0f, 0.0f, 0.0f};
     Vec3 accel_bias = {0.0f, 0.0f, 0.0f};
     Vec3 last_gyro = {0.0f, 0.0f, 0.0f};
-    Vec3 last_ accel = {0.0f, 0.0f, 0.0f};
+    Vec3 last_accel = {0.0f, 0.0f, 0.0f};
     unsigned long lasttime = 0;
+
+    sensors_event_t accel_event;
+    sensors_event_t gyro_event;
+    sensors_event_t temp_event;
 };
 
